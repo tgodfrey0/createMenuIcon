16d19887278018970243edb057b3a316b8ceea2f
tgodfrey0 <toby.godfrey2003@gmail.com>
2019-03-15 17:00:16 +0000

    Added installation files

---
+++ apt-install.sh
@@ -0,0 +1,11 @@
+echo ""
+echo "Installing G++"
+sudo apt-get install g++
+echo ""
+echo "G++ has been installed"
+echo ""
+echo "Compiling main file"
+g++ ./createDesktopIcon.cpp -o ./createDesktopIcon
+echo ""
+echo "File has been compiled"
+chmod +u ./createDesktopIcon
