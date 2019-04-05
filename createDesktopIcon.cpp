c80369ddc40fc233cde83df8429a1974c137187b
tgodfrey0 <41199219+tgodfrey0@users.noreply.github.com>
2019-04-05 16:31:41 +0100

    Edited main file

--- createDesktopIcon.cpp
+++ createDesktopIcon.cpp
@@ -1,45 +1,36 @@
-//Author: tgodfrey0
-
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
-#include <boost/range/algorithm/remove_if.hpp>
 
 using namespace std;
 
-void execCommand(string command)
-{
+void execCommand(string command) {
 	cout << "Executing command: \"" + command + "\"" << endl;
 	system(command.c_str());
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
 
-	string paramVal;
-	string name;
-	string comment;
-	string executable;
-	string icon;
-	string type;
-	string genericName;
-	string notify;
-	string categories;
+	string paramVal = "";
+	string name = "";
+	string comment = "";
+	string executable = "";
+	string icon = "";
+	string type = "";
+	string genericName = "";
+	string notify = "";
+	string categories = "";
 
-	for (int i = 1; i < argc; i++)
-	{
+	for(int i = 1; i < argc; i++) {
 		string param = argv[i];
-		if (i + 1 <= argc - 1)
-		{
-			paramVal = argv[i + 1];
+		if(i+1 <= argc-1) {
+			paramVal = argv[i+1];
 		}
-		else
-		{
+		else {
 			paramVal = argv[i];
 		}
-		if (param == "-h")
-		{
+		if(param == "-h") {
 			cout << "-h        Display this help dialogue" << endl;
 			cout << "-n        Set entry name" << endl;
 			cout << "-c        Set entry comment" << endl;
@@ -50,61 +41,52 @@ int main(int argc, char *argv[])
 			cout << "-f        Set entry categories (end each category with ;)" << endl;
 			cout << "Example:" << endl;
 			cout << "./createDesktopIcon -n Ghidra -c \"An NSA decomplier\" -e ./ghidraRun -i ./ghidraIcon -t Application -g Decompiler -s false -f \"Utility;TextEditor;Development;\"" << endl;
-			return (0);
+			return(0);
 		}
-		else if (param == "-n")
-		{
+		else if(param == "-n") {
 			name = paramVal;
 			i++;
 			continue;
 		}
-		else if (param == "-c")
-		{
+		else if(param == "-c") {
 			comment = paramVal;
 			i++;
 			continue;
 		}
-		else if (param == "-e")
-		{
+		else if(param == "-e") {
 			executable = paramVal;
 			i++;
 			continue;
 		}
-		else if (param == "-i")
-		{
+		else if(param == "-i") {
 			icon = paramVal;
 			i++;
 			continue;
 		}
-		else if (param == "-g")
-		{
+		else if(param == "-g") {
 			genericName = paramVal;
 			i++;
 			continue;
 		}
-		else if (param == "-s")
-		{
+		else if(param == "-s") {
 			notify = paramVal;
 			i++;
 			continue;
 		}
-		else if (param == "-f")
-		{
+		else if(param == "-f") {
 			categories = paramVal;
 			i++;
 			continue;
 		}
-		else
-		{
+		else {
 			cout << "ERROR >>> Unrecognised parameter " << param << endl;
 		}
 	}
 
-	string title = name;
-	title.erase(boost::remove_if(title, ::isspace), title.end());
+
 	string extension = ".desktop";
 	string path = "/usr/share/applications/";
-	string fullPath = path + title + extension;
+	string fullPath = path + name + extension;
 
 	string command0 = ("sudo touch " + fullPath);
 	string command1 = ("sudo echo \"[Desktop Entry]\" >> " + fullPath);
@@ -118,17 +100,35 @@ int main(int argc, char *argv[])
 	string command9 = ("sudo echo \"Categories=" + categories + "\" >> " + fullPath);
 	string command10 = ("sudo echo \"MimeType=text/plain;inode/directory;\" >> " + fullPath);
 
-	execCommand(command0);
-	execCommand(command1);
-	execCommand(command2);
-	execCommand(command3);
-	execCommand(command4);
-	execCommand(command5);
-	execCommand(command6);
-	execCommand(command7);
-	execCommand(command8);
-	execCommand(command9);
+	if(name == "" || executable == ""){
+		cout << "ERROR: Name or path not defined" << endl;
+		return(1);
+	}
+	if(name != ""){
+		execCommand(command0);
+		execCommand(command1);
+		execCommand(command2);
+	}
+	if(comment != ""){	
+		execCommand(command3);
+	}
+	if(genericName != ""){	
+		execCommand(command4);
+	}
+	if(executable != ""){	
+		execCommand(command5);
+	}
+	if(icon != ""){	
+		execCommand(command6);
+	}		
+	execCommand(command7);	
+	if(notify != ""){	
+		execCommand(command8);
+	}
+	if(categories != ""){	
+		execCommand(command9);
+	}
 	execCommand(command10);
 
-	return (0);
+	return(0);
 }
